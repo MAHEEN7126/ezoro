f64ab20810843201e0a30c068c979f3d94c1f788
MAHEEN7126 <maheenbano252@gmail.com>
2025-07-27 21:14:08 +0500

    removed video and added image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 function Header() {
   return (
-    <header className="w-full h-screen ">
+    <header className="w-full h-screen">
       <div className="realtive w-full overflow-hidden h-[100px]">
+        <img
+          loading="lazy"
+          src="/assets/CoverPics/ideogram-v3-quality_create_a_1_standing_ (5).png"
+          alt="womanpic"
+          className="absolute w-full h-full object-cover top-3 object-[20%_40%] h-auto rounded-1xl"
+        />
+
         <h1 className="animate-scroll whitespace-nowrap text-6xl sm:text-7xl md:text-7xl font-bold tracking-wide text-[#1a1a1a] my-4 sm:my-5 transition-all duration-700 hover: tracking-[0.4rem] hover:text-[#5e4b8b]">
           <span className="inline-block px-4">
             ~Every thread tells a secret
@@ -9,21 +16,10 @@ function Header() {
           <span className="inline-block px-4">
             ~Every thread tells a secret
           </span>
-          <span className="inline-block px-4">
+          <span className="inline-block px-1">
             ~Every thread tells a secret
           </span>
         </h1>
-      </div>
-      <div className=" h-full ">
-        <video
-          autoPlay
-          loop
-          playsInline
-          muted
-          src="/assets/CoverPics/6009714_4k_Video_Attractive_3840x2160 (1).mp4"
-          type="video/mp4"
-          className="w-full h-full object-cover"
-        />
       </div>
     </header>
   );
