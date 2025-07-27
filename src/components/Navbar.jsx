a2d562c370cc9e860d8990116de19df319e42492
MAHEEN7126 <maheenbano252@gmail.com>
2025-07-27 15:47:01 +0500

    Updated Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="w-full h-[53px] bg-white/30 backdrop-blur-md text-black font-normal shadow-md py-4 px-6 sm:px-10 flex flex-wrap justify-center gap-4 sm:gap-8 text-base sm:text-lg fixed top-0 z-50 overflow-hidden">
+    <nav className="w-full h-[53px] bg-white/30 backdrop-blur-md text-black font-bold shadow-md py-4 px-4 sm:px-10 flex flex-wrap justify-center gap-9 sm:gap-20 text-base sm:text-lg fixed top-0 z-50 overflow-hidden">
       <Link to="/">Home</Link>
       <Link to="Clothes">Clothes</Link>
       <Link to="Jewelry">Jewelry</Link>
