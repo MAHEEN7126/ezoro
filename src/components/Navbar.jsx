5d36455f7f26a083668d5f43534cd319e2ca0100
MAHEEN7126 <maheenbano252@gmail.com>
2025-07-28 13:56:21 +0500

    added logoand made navbar good looking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,54 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="w-full h-[53px] bg-white/30 backdrop-blur-md text-black font-bold shadow-md py-4 px-4 sm:px-10 flex flex-wrap justify-center gap-9 sm:gap-20 text-base sm:text-lg fixed top-0 z-50 overflow-hidden">
-      <Link to="/">Home</Link>
-      <Link to="Clothes">Clothes</Link>
-      <Link to="Jewelry">Jewelry</Link>
-      <Link to="AllShoes">Shoes</Link>
-      <Link to="Profile">Profile</Link>
-      <Link to="Cart">cart</Link>
-      <Link to="Signup">Signup</Link>
+    <nav className=" w-full flex items-center justify-between  py-1 bg-white shadow-md">
+      <div
+        className="w-full h-[53px] bg-white/30 backdrop-blur-md text-blackshadow-xl font-bold 
+          Shoes py-4 px-4 sm:px-10 flex flex-wrap justify-center gap-9 sm:gap-20 text-base sm:text-lg fixed top-0 z-50 overflow-hidden"
+      >
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="Clothes"
+        >
+          Clothes
+        </Link>
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="Jewelry"
+        >
+          Jewelry
+        </Link>
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="AllShoes"
+        >
+          Shoes
+        </Link>
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="Profile"
+        >
+          Profile
+        </Link>
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="Cart"
+        >
+          cart
+        </Link>
+        <Link
+          className="transition duration-300 transform hover:shadow-sm hover:scale-110 active:scale-110"
+          to="Signup"
+        >
+          Signup
+        </Link>
+      </div>
     </nav>
   );
 }
