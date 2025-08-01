dec0d5b874ea87c272b53d1fa1370a4fe39253b0
MAHEEN7126 <maheenbano252@gmail.com>
2025-08-01 12:05:32 +0500

    correcting image size

--- src/pages/Clothes.jsx
+++ src/pages/Clothes.jsx
@@ -4,17 +4,16 @@ function Clothes() {
   return (
     <div className="bg-gray-200">
       <div>
-        <header className="w-full mt-[37px] overflow-hidden relative h-[200px] sm:h-[300px] lg:h-[300px]">
+        <header className="relative w-full h-[250px] sm:h-[320px] lg:h-[400px] overflow-hidden">
           <img
-            loading="lazy"
             src="/assets/CoverPics/beautiful-blonde-woman-blue-light.jpg"
             alt="womanpic"
-            className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
+            className="absolute inset-0 w-full h-full object-cover scale-125 object-center"
           />
-          <h2 className="text-white whitespace-nowrap absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl font-bold font-serif tracking-wide">
+          <h2 className="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl sm:text-4xl font-bold font-serif tracking-wide">
             Pure Elegance
           </h2>
-          <h2 className="text-white absolute top-[65%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl sm:text-2xl font-serif tracking-wide">
+          <h2 className="text-white absolute top-[65%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-sm sm:text-xl font-serif tracking-wide">
             Where elegance isn't worn, it's lived.
           </h2>
         </header>
